48d39766ccee56b591fcc329e7aa340f2f188987
web <web@webdeMacBook-Pro.local>
2018-10-08 00:49:12 +0800

    update

--- src/components/containerTab.vue
+++ src/components/containerTab.vue
@@ -1,16 +1,49 @@
 <template>
     <div class="hello">
-        <Tab/>
+        <div>
+            <div class="tabs-bar">
+                <tab v-model="activeKey">
+                    <pane label="标签1" name="1">
+                        1111
+                    </pane>
+                    <pane label="标签2" name="2">
+                        2222
+                    </pane>
+                    <pane label="标签3" name="3">
+                        33333
+                    </pane>
+                </tab>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import Bus from '@/bus/bus.js'
-import Tab from './tab' 
+import pane from './pane' 
+import tab from './tab'
 export default {
     name: 'containerTab',
+    data(){
+        return {
+            activeKey: '1'
+        }
+    },
     components: {
-        Tab
+        pane,
+        tab
+    },
+    watch: {
+        activeKey(val){
+            console.log(val,'这是watch -> activeKey的变化')
+        }
+    },
+    mounted(){
+        
+        /**
+         * 防止重复添加出现的问题
+         * 所以不再 panne 组件 的 mounted 调用 updateNav
+         */
+        this.$children[0].$children[0].updateNav()
     }
 }
 </script>
---
+++ src/components/pane.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="hello" v-show="show">
+        <slot>
+        </slot>
+    </div>
+</template>
+
+<script>
+import Bus from '@/bus/bus.js'
+import Tab from './tab' 
+export default {
+    name: 'pane',
+    props: ['label','name'],
+    data(){
+        return {
+            show: true
+        }
+    },
+    methods:{
+        updateNav(){
+            this.$parent.updateNav()
+        }
+    },
+    watch: {
+        lebel(){
+            this.updateNav()
+        }
+    },
+    mounted(){
+        // this.updateNav()
+    },
+    components: {
+        Tab
+    }
+}
+</script>
+
+<style scoped>
+</style>
