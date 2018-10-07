4a057d7881fea917773f1a318ecd8a1a0d876dff
web <web@webdeMacBook-Pro.local>
2018-10-07 14:26:06 +0800

    update component

---
+++ src/components/containerTab.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello">
+        <Tab/>
+    </div>
+</template>
+
+<script>
+import Bus from '@/bus/bus.js'
+import Tab from './tab' 
+export default {
+    name: 'containerTab',
+    components: {
+        Tab
+    }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/tab.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="hello">
+        2134
+    </div>
+</template>
+
+<script>
+import Bus from '@/bus/bus.js'
+export default {
+    name: 'tab',
+}
+</script>
+
+<style scoped>
+</style>
