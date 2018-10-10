33891c65de44b1ec68b66497cff6aedf5608d22a
webgzh907247189 <18255106634@163.com>
2018-10-11 02:02:28 +0800

    1

--- src/components/ClickOutsideHead.vue
+++ src/components/ClickOutsideHead.vue
@@ -1,6 +1,8 @@
 <template>
   	<div class="hello">
   		<div class="demo" @click="headClick">66666666666666</div>
+  		<div v-time="timeNow" class="time-directive"></div>
+  		<div v-time="timeBefore" class="time-directive"></div>
     	<slot>
       	
      	</slot>
@@ -10,6 +12,12 @@
 <script>
 export default {
   	name: 'ClickOutsideHead',
+  	data(){
+  		return {
+  			timeNow: 1539193587165,
+  			timeBefore: 1488930695721
+  		}
+  	},
   	methods: {
   		headClick(){
   			this.$emit('outsideHeadClick')
@@ -28,4 +36,10 @@ export default {
 	margin-bottom: 10px;
 }
 
+.time-directive{
+	width: 200px;
+	height: 18px;
+	background: #967;
+	margin-bottom: 2px;
+}
 </style>
